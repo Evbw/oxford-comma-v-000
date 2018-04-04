9ee0eeada5fce42943e28d8d3168c36cd2bb16ed
Everett <everett.bowline@gmail.com>
2018-04-04 23:38:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,8 @@
-class Array
-  def join_all(join_with = ", ", connector = "and", last_comma = false)
-    return self.to_s if self.empty? || self.size==1
-    connector = join_with+connector if last_comma
-    [list[0...-1].join(join_with), list.last].join(connector)
-  end
-end
-
 def oxford_comma(array)
+  if array.length == 2
+    "#{array[0]} and #{array[1]}"
+    elsif 2 < array.length
+    array
+  end  
   array.join(", ")
 end
