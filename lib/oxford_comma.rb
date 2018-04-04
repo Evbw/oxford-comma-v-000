74081d5aa6e8e751bbaffff5157074ec9464970a
Everett <everett.bowline@gmail.com>
2018-04-04 23:20:49 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
 def oxford_comma(array)
-  array.join(", ") << ", and #{array[-1]}"
+    def join_all(join_with = ", ", connector = "and", last_comma = false)
+    return self.to_s if self.empty? || self.size==1
+    connector = join_with+connector if last_comma
+    [list[0...-1].join(join_with), list.last].join(connector)
+  end
 end
