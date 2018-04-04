6f1f160553fd6fb687be2a8c2ed8995c68184f46
Everett <everett.bowline@gmail.com>
2018-04-04 23:27:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,7 @@
 def oxford_comma(array)
-  if array.length == 1
-    puts "#{array[0]}"
-  elsif array.length == 2
+  if array.length == 2
     puts "#{array[0]} and #{array[1]}"
-  else
+  elsif
     array[-1].join(" and ")
   end
   array.join(", ")
